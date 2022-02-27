5f305b947390f3d069bdcf2a413f70acb93fc0d2
Grigoriy Kolenko <kolenko@me.com>
2022-02-27 22:39:42 +0100

    Easy peasy

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,12 +1,43 @@
 import { NextPage } from "next"
-import { Button } from "ui"
+import Head from "next/head"
+import { useEffect, useState } from "react"
+import { Button, Container } from "ui"
 import { HomeLayout } from "../components"
 
 const Home: NextPage = () => {
+  const [text, setText] = useState("")
+  const [kadyrovshina, setKadyrovshina] = useState([])
+  useEffect(() => {
+    const textArr = text.split(" ")
+    const randIndexes = Array.from(
+      new Set(Array.from({ length: textArr.length }, () => Math.floor(Math.random() * textArr.length)))
+    )
+    randIndexes.forEach((index) => {
+      textArr.splice(index, 0, "дон")
+    })
+    setKadyrovshina(textArr)
+  }, [text])
   return (
     <HomeLayout>
-      <h1>Kadyrovshina</h1>
-      <Button>test</Button>
+      <Head>
+        <title>Любой текст в кадыровщину</title>
+      </Head>
+      <Container>
+        <h1 className="text-xl">Любой текст в кадыровщину</h1>
+        <div className="grid grid-cols-2 gap-4">
+          <textarea
+            className="border rounded p-4"
+            name="simpletext"
+            id="simpletext"
+            cols={100}
+            rows={30}
+            onChange={(e) => setText(e.target.value)}
+          >
+            {text}
+          </textarea>
+          <div className="p-4">{kadyrovshina.join(" ")}</div>
+        </div>
+      </Container>
     </HomeLayout>
   )
 }
